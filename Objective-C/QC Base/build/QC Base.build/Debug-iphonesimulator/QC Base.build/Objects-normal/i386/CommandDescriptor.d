4d26288255ecaefc888795e1e89e3d91e67daa93
Lee Barney <lee@337Pro.local>
2012-09-22 23:15:51 -0600

    Removed build files

--- Objective-C/QC Base/build/QC Base.build/Debug-iphonesimulator/QC Base.build/Objects-normal/i386/CommandDescriptor.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/lee/qc_github_repo/qcnative/Objective-C/QC\ Base/QC\ Base/CommandDescriptor.m \
-  /Users/lee/qc_github_repo/qcnative/Objective-C/QC\ Base/QC\ Base/CommandDescriptor.h
