93cd1532b0b6c92c8a776b1a5445f6812eaa83a3
Lee Barney <lee@337Pro.local>
2012-09-22 23:16:21 -0600

    Removing deleted files

--- Objective-C/QC Base/build/QC Base.build/Debug-iphonesimulator/QC Base.build/Objects-normal/i386/ControlObjectStack.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/lee/qc_github_repo/qcnative/Objective-C/QC\ Base/QC\ Base/ControlObjectStack.m \
-  /Users/lee/qc_github_repo/qcnative/Objective-C/QC\ Base/QC\ Base/ControlObjectStack.h \
-  /Users/lee/qc_github_repo/qcnative/Objective-C/QC\ Base/QC\ Base/QCMapper.h \
-  /Users/lee/qc_github_repo/qcnative/Objective-C/QC\ Base/QC\ Base/QCControlObject.h \
-  /Users/lee/qc_github_repo/qcnative/Objective-C/QC\ Base/QC\ Base/QCReturnValues.h \
-  /Users/lee/qc_github_repo/qcnative/Objective-C/QC\ Base/QC\ Base/StackWaitMonitor.h
